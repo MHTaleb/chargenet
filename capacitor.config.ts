35a0e63816f2df30568902fee67e36945098d6c4
mhtaleb <talcorpdz@gmail.com>
2024-08-31 16:06:21 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ChargeNet',
+  webDir: 'www'
+};
+
+export default config;
